0d8ad13d7f36bfce28cdc5eaa656597099827a6e
Stephon Brown <stephonb14@gmail.com>
2020-07-22 03:21:01 -0400

    Updates

---
+++ precache-manifest.3d628dc4788fda73f39daee6885f5966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502fa2af572755c0453f0af6d8d32f43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d152c4bd5e0827f81d96",
+    "url": "/static/css/main.d54e0761.chunk.css"
+  },
+  {
+    "revision": "7dea5a15b9f533a6324f",
+    "url": "/static/js/2.95bf7b38.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/static/js/2.95bf7b38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d152c4bd5e0827f81d96",
+    "url": "/static/js/main.6f420b5d.chunk.js"
+  },
+  {
+    "revision": "d744378191b0975f6d8e",
+    "url": "/static/js/runtime-main.bd1c6778.js"
+  }
+]);
